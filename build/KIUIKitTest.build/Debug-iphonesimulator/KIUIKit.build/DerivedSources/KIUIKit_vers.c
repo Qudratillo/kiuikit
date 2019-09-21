ef0447263986915b990e7429de12d2efdf3302c0
Kudratillo Ismatov <qudratillo95@gmail.com>
2019-09-21 14:39:20 +0500

    gitignore fix

--- build/KIUIKitTest.build/Debug-iphonesimulator/KIUIKit.build/DerivedSources/KIUIKit_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char KIUIKitVersionString[];
- extern const double KIUIKitVersionNumber;
-
- const unsigned char KIUIKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:KIUIKit  PROJECT:KIUIKitTest-1" "\n";
- const double KIUIKitVersionNumber __attribute__ ((used)) = (double)1.;
--- build/KIUIKitTest.build/Debug-iphonesimulator/KIUIKit.build/Objects-normal/i386/KIUIKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kudratilloismatov/Desktop/Projects/iOS/KIUIKitTest/build/KIUIKitTest.build/Debug-iphonesimulator/KIUIKit.build/DerivedSources/KIUIKit_vers.c
